db8bdcc9d2d46af1d87a394571af8438488cfe17
Wolfgang Loder <kujuasiokubahatisha@gmail.com>
2019-05-03 17:04:03 +0100

    broadway review skeleton

---
+++ reviews/broadway/broadway_rabbitmq/lib/broadway_rabbitmq.ex
@@ -0,0 +1,48 @@
+defmodule BroadwayRabbitmq do
+  defmodule MyBroadway do
+  use Broadway
+
+  alias Broadway.Message
+
+  def start_link(_opts) do
+    Broadway.start_link(__MODULE__,
+      name: RabbitBroadway,
+      producers: [
+        default: [
+          module: {BroadwayRabbitMQ.Producer,
+            queue: "my_queue",
+            qos: [
+              prefetch_count: 50,
+            ]
+          },
+          stages: 2
+        ]
+      ],
+      processors: [
+        default: [
+          stages: 50
+        ]
+      ],
+      batchers: [
+        default: [
+          batch_size: 10,
+          batch_timeout: 1500,
+          stages: 5
+        ]
+      ]
+    )
+  end
+
+  def handle_message(_, message, _) do
+    message
+    |> Message.update_data(fn data -> {data, String.to_integer(data) * 2} end)
+  end
+
+  def handle_batch(_, messages, _, _) do
+    list = messages |> Enum.map(fn e -> e.data end)
+    IO.inspect(list, label: "Got batch")
+    messages
+  end
+
+end
+end
---
+++ reviews/broadway/broadway_rabbitmq/test/broadway_rabbitmq_test.exs
@@ -0,0 +1,7 @@
+defmodule BroadwayRabbitmqTest do
+  use ExUnit.Case
+
+  test "dummy" do
+    assert true
+  end
+end
